154a27690bff9141c886936ce7ce724daa0b8878
Alex McWhirter <alex@triadic.us>
2022-05-31 12:55:20 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+import { utils } from "ethers";
+import { MerkleTree } from "merkletreejs";
+
+import keccak256 from "keccak256";
+import fs from "fs";
+ 
+const Airdrop = JSON.parse(fs.readFileSync('airdrop-balances.json'));
+ 
+let elements = []
+ 
+for (const [k, v] of Object.entries(Airdrop)) {
+    elements.push(
+        utils.solidityKeccak256(["address", "uint256"], [k, v])
+    )
+  }
+ 
+const merkleTree = new MerkleTree(elements, keccak256, { sort: true });
+ 
+const root = merkleTree.getHexRoot();
+
+const expectedRoot = "0x8f4e1c18aa0323d567b9abc6cf64f9626e82ef1b41a404b3f48bfa92eecb9142";
+
+if (root != expectedRoot) {
+    console.log("Calculated Merkle Root does not match the PLSD contract!");
+    console.log("Calculated Root:", root);
+    console.log("Expected Root:", expectedRoot);
+}
+
+else {
+    console.log("Merkel Root Verified:", root);
+}
